28540b9f8cb8d69bdb283b0119f14a57e8d213ba
Irina <namdykova03@gmail.com>
2024-03-04 15:55:48 +0300

    add Task_001

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке 
+// от M до N. Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+    int M = ReadInt("Введите число M: ");
+    int N = ReadInt("введите число N: ");
+    PrintNumbers(M, N);
+}
+
+void PrintNumbers(int M, int N)
+{
+    if (N < M) return;
+    PrintNumbers(M, N - 1);
+    System.Console.Write(N + " ");
+}
+
+int ReadInt(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+Main();
